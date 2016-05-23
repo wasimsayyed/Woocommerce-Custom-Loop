ae913193c91dd3b3c14c51777a92ce28d0f7c526
Wasim Sayyed <wasimsayyed@users.noreply.github.com>
2016-05-23 19:15:40 +0530

    Update woocommerce-loop.php
    Added Cart button in loop

--- woocommerce-loop.php
+++ woocommerce-loop.php
@@ -30,6 +30,19 @@
           <td><?php the_title(); ?></td>
           <td><?php echo ( $sku = $product->get_sku() ) ? $sku : __( 'N/A', 'woocommerce' ); ?></td>
           <td><?php   echo $product->get_price_html(); ?></td>
+          <td>
+           <?php global $product; // For Adding Add to Cart button in loop
+              echo apply_filters( 'woocommerce_loop_add_to_cart_link',
+              sprintf( '<a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" class="button %s product_type_%s">%s</a>',
+              esc_url( $product->add_to_cart_url() ),
+              esc_attr( $product->id ),
+              esc_attr( $product->get_sku() ),
+              $product->is_purchasable() ? 'add_to_cart_button' : '',
+              esc_attr( $product->product_type ),
+              esc_html( $product->add_to_cart_text() )
+              ), $product ); 
+            ?>
+        </td>
         </tr>
         </table>
         <?php
